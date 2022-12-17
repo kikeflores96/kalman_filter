4a68da7e6d38200351685d6ffe5689102435af5f
kikeflores96 <enriquefloresmontoya1996@gmail.com>
2022-12-17 13:06:26 +0100

    Commit before transfer to MAC

--- lib/attitude/attitude.cpp
+++ lib/attitude/attitude.cpp
@@ -2,20 +2,5 @@
 #include <math.h>
 
 
-void eye(double *M, int dim, double diagonal){
-    for(int i= 0; i<dim; i++){
-      *M = diagonal;
-      M+=dim;
-  }
-};
 
-
-
-
-attitude :: attitude{
-
-  eye(P, sizeof(P)/sizeof(P[0]), 0.1);
-
-
-}
 
